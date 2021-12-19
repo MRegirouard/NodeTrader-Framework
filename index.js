6187da69932f09b90d9c79cea2cad06947deabec
Ethan Girouard <mr.egirouard@gmail.com>
2021-12-19 17:32:01 -0500

    Add argument parsing

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ const log = require('./customLog.js')
 log.info('')
 log.info('Starting NodeTrader Framework')
 log.info('')
+
+log.debug('Parsing command line arguments...')
+
+const { ArgumentParser } = require('argparse')
+
+const parser = new ArgumentParser({ description: 'A Node.js cryptocurrency trading bot framework.' })
+
+const args = parser.parse_args()
+
+log.debug('Arguments parsed.')
+log.debug('Arguments:', args)
