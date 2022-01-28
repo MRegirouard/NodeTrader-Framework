44f621ce7af8b981804528ff50d0c8b01557ecde
Ethan Girouard <mr.egirouard@gmail.com>
2022-01-27 21:04:43 -0500

    Add help command and command switch

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ log.debug('Parsing command line arguments...')
 
 const { ArgumentParser } = require('argparse')
 
-const commands = []
+const commands = ['help']
 
 const parser = new ArgumentParser({ description: 'A Node.js cryptocurrency trading bot framework.' })
 parser.add_argument('command', { choices: commands, help: 'Command to execute.' })
@@ -18,3 +18,14 @@ const args = parser.parse_args()
 log.debug('Arguments parsed.')
 log.debug('Arguments: ' + args)
 log.verbose('Running command: ' + args.command)
+
+switch (args.command)
+{
+	case 'help':
+	default:
+		log.verbose('Printing command help text...')
+		parser.print_help()
+		log.debug('Printed command help text. Exiting program with code 0.')
+		process.exit(0)
+		break;
+}
