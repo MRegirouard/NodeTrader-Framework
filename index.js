aa51fb7f6b29b972622b36eff148662acfcbd0f2
Ethan Girouard <mr.egirouard@gmail.com>
2022-01-27 20:56:49 -0500

    Add command positional argument

--- index.js
+++ index.js
@@ -8,9 +8,13 @@ log.debug('Parsing command line arguments...')
 
 const { ArgumentParser } = require('argparse')
 
+const commands = []
+
 const parser = new ArgumentParser({ description: 'A Node.js cryptocurrency trading bot framework.' })
+parser.add_argument('command', { choices: commands, help: 'Command to execute.' })
 
 const args = parser.parse_args()
 
 log.debug('Arguments parsed.')
 log.debug('Arguments: ' + args)
+log.verbose('Running command: ' + args.command)
