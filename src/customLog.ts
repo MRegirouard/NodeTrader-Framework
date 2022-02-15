8e459d48601115287459eaf5d61c80f470711f65
Ethan Girouard <mr.egirouard@gmail.com>
2022-02-14 19:35:32 -0500

    Remove date from log timestamp, add millis

--- src/customLog.ts
+++ src/customLog.ts
@@ -15,7 +15,9 @@ const customFormat = winston.format.printf((info : winston.LogEntry) =>
 
 	info.level = info.level.padStart(info.level.length + Math.floor((spaceVal - info.level.length) / 2), ' ')
 	info.level = info.level.padEnd(spaceVal, ' ')
-	info.timestamp = new Date().toLocaleString('en-US', { year: '2-digit', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit' })
+	const date = new Date()
+	info.timestamp = date.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', second: '2-digit' })
+	info.timestamp = info.timestamp.slice(0, -3) + '.' + date.getMilliseconds().toString().padStart(3, '0') + info.timestamp.slice(-3)
 
 	if (info.tradingName) // Logs from trading algorithms should display the trading name
 	{
