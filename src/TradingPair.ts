2bfe9e643a66922863d8021146dbb5d344189884
Ethan Girouard <mr.egirouard@gmail.com>
2022-08-16 16:58:00 -0400

    Write TradingPair interface

---
+++ src/TradingPair.ts
@@ -0,0 +1,10 @@
+/**
+ * Represents a trading pair, for buying and selling assets. Should contain the symbol of each asset, not the name.
+ */
+interface TradingPair
+{
+    base: string // The base asset symbol of the trading pair. For example, 'USDT' for Tether.
+    trade: string // The trade asset symbol of the trading pair. For example, 'BTC' for Bitcoin.
+}
+
+export default TradingPair
