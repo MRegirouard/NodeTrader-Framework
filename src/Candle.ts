7359c84b15faffad004d1d30331d2f6d7a98b854
Ethan Girouard <mr.egirouard@gmail.com>
2022-09-05 16:24:23 -0400

    Add Candle.fromArr
    Write unit test

--- src/Candle.ts
+++ src/Candle.ts
@@ -18,6 +18,17 @@ class Candle
 		public readonly high: number, public readonly low: number,
 		public readonly close: number, public readonly volume: number)
 	{}
+
+	/**
+	 * Creates a new Candle from an array of numbers
+	 * @param arr The array of numbers to create the candle from, in the following order:
+	 * [date (in milliseconds), open, high, low, close, volume]
+	 * @returns The new Candle
+	 */
+	public static fromArr(arr: [number, number, number, number, number, number]) : Candle
+	{
+		return new Candle(moment(arr[0]), arr[1], arr[2], arr[3], arr[4], arr[5])
+	}
 }
 
 export default Candle
--- test/Candle.test.ts
+++ test/Candle.test.ts
@@ -22,3 +22,25 @@ describe('Candle', () =>
         expect(candle.volume).toBe(volume)
     })
 })
+
+describe('Candle.fromArr', () =>
+{
+    test('should create a new Candle from an array of numbers', () =>
+    {
+        const date: moment.Moment = moment()
+        const open: number = 15.18
+        const high: number = 15.27
+        const low: number = 15.17
+        const close: number = 15.25
+        const volume: number = 1689
+
+        const candle = Candle.fromArr([date.valueOf(), open, high, low, close, volume])
+        expect(candle).toBeInstanceOf(Candle)
+        expect(candle.date.valueOf()).toBe(date.valueOf())
+        expect(candle.open).toBe(open)
+        expect(candle.high).toBe(high)
+        expect(candle.low).toBe(low)
+        expect(candle.close).toBe(close)
+        expect(candle.volume).toBe(volume)
+    })
+})
