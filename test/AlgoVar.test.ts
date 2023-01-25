78b64dd25695c536809515c5ccdfd8d8d78cfe8f
Ethan Girouard <mr.egirouard@gmail.com>
2023-01-25 12:16:47 -0500

    Lint tests
    Add tsconfig for tests

--- test/AlgoVar.test.ts
+++ test/AlgoVar.test.ts
@@ -1,85 +1,85 @@
-import AlgoVar from "../src/AlgoVar"
-import { VarChangedEvent } from "../src/AlgoVar"
-import { EventEmitter } from "events"
+import { AlgoVar, VarChangedEvent } from '../src/AlgoVar'
+import { EventEmitter } from 'events'
+import { describe, expect, test, jest } from '@jest/globals'
 
 describe('AlgoVar', () =>
 {
-    test('should be able to have a number type', () =>
-    {
-        const algoVar = new AlgoVar<number>('myVar', 100)
-        expect(algoVar)
-        expect(algoVar).toBeInstanceOf(EventEmitter)
-        expect(algoVar.value).toBe(100)
-        expect(algoVar.name).toBe('myVar')
-    })
+	test('should be able to have a number type', () =>
+	{
+		const algoVar = new AlgoVar<number>('myVar', 100)
+		expect(algoVar)
+		expect(algoVar).toBeInstanceOf(EventEmitter)
+		expect(algoVar.value).toBe(100)
+		expect(algoVar.name).toBe('myVar')
+	})
 
-    test('should be able to have string type', () =>
-    {
-        const algoVarStr = new AlgoVar<string>('myStrVar', 'str')
-        expect(algoVarStr)
-        expect(algoVarStr.value).toBe('str')
-        expect(algoVarStr.name).toBe('myStrVar')
-    })
+	test('should be able to have string type', () =>
+	{
+		const algoVarStr = new AlgoVar<string>('myStrVar', 'str')
+		expect(algoVarStr)
+		expect(algoVarStr.value).toBe('str')
+		expect(algoVarStr.name).toBe('myStrVar')
+	})
 
-    test('should be able to have Date type', () =>
-    {
-        const algoVarDate = new AlgoVar<Date>('myDateVar', new Date())
-        expect(algoVarDate)
-        expect(algoVarDate.value).toBeInstanceOf(Date)
-        expect(algoVarDate.name).toBe('myDateVar')
-    })
+	test('should be able to have Date type', () =>
+	{
+		const algoVarDate = new AlgoVar<Date>('myDateVar', new Date())
+		expect(algoVarDate)
+		expect(algoVarDate.value).toBeInstanceOf(Date)
+		expect(algoVarDate.name).toBe('myDateVar')
+	})
 })
 
 describe('VarChangedEvent', () =>
 {
-    test('should be able to create a VarChangedEvent', () =>
-    {
-        const algoVar = new AlgoVar<number>('myVar', 100)
-        const date = new Date()
-        const varChangedEvent = new VarChangedEvent(algoVar, 100, 200, date)
-        expect(varChangedEvent)
-        expect(varChangedEvent.algoVar).toBe(algoVar)
-        expect(varChangedEvent.oldValue).toBe(100)
-        expect(varChangedEvent.newValue).toBe(200)
-        expect(varChangedEvent.date).toBe(date)
-    })
+	test('should be able to create a VarChangedEvent', () =>
+	{
+		const algoVar = new AlgoVar<number>('myVar', 100)
+		const date = new Date()
+		const varChangedEvent = new VarChangedEvent(algoVar, 100, 200, date)
+		expect(varChangedEvent)
+		expect(varChangedEvent.algoVar).toBe(algoVar)
+		expect(varChangedEvent.oldValue).toBe(100)
+		expect(varChangedEvent.newValue).toBe(200)
+		expect(varChangedEvent.date).toBe(date)
+	})
 
-    test('should automatically set the newValue and date if not provided', () =>
-    {
-        const algoVar = new AlgoVar<number>('myVar', 200)
-        const varChangedEvent = new VarChangedEvent(algoVar, 100)
-        expect(varChangedEvent)
-        expect(varChangedEvent.algoVar).toBe(algoVar)
-        expect(varChangedEvent.oldValue).toBe(100)
-        expect(varChangedEvent.newValue).toBe(200)
-        expect(varChangedEvent.date).toBeInstanceOf(Date)
-    })
+	test('should automatically set the newValue and date if not provided', () =>
+	{
+		const algoVar = new AlgoVar<number>('myVar', 200)
+		const varChangedEvent = new VarChangedEvent(algoVar, 100)
+		expect(varChangedEvent)
+		expect(varChangedEvent.algoVar).toBe(algoVar)
+		expect(varChangedEvent.oldValue).toBe(100)
+		expect(varChangedEvent.newValue).toBe(200)
+		expect(varChangedEvent.date).toBeInstanceOf(Date)
+	})
 })
 
 describe('AlgoVar.value', () =>
 {
-    test('should set/get the value of the AlgoVar', () =>
-    {
-        const algoVar = new AlgoVar('myVar', 100)
-        algoVar.value = 200
-        expect(algoVar)
-        expect(algoVar.value).toBe(200)
-    })
+	test('should set/get the value of the AlgoVar', () =>
+	{
+		const algoVar = new AlgoVar('myVar', 100)
+		algoVar.value = 200
+		expect(algoVar)
+		expect(algoVar.value).toBe(200)
+	})
 
-    test('should emit a changed event when the value is changed', () =>
-    {
-        const algoVar = new AlgoVar('myVar', 100)
-        const callback = jest.fn((event: VarChangedEvent<number>) =>
-        {
-            expect(event)
-            expect(event.algoVar).toBe(algoVar)
-            expect(event.oldValue).toBe(100)
-            expect(event.newValue).toBe(200)
-            expect(event.date).toBeInstanceOf(Date)
-        })
+	test('should emit a changed event when the value is changed', () =>
+	{
+		const algoVar = new AlgoVar('myVar', 100)
+		const callback = jest.fn((event: VarChangedEvent<number>) =>
+		{
+			expect(event)
+			expect(event.algoVar).toBe(algoVar)
+			expect(event.oldValue).toBe(100)
+			expect(event.newValue).toBe(200)
+			expect(event.date).toBeInstanceOf(Date)
+		})
 
-        algoVar.on('changed', callback)
-        algoVar.value = 200
-        expect(callback).toHaveBeenCalled()
-    })
+		algoVar.on('changed', callback)
+		algoVar.value = 200
+		expect(callback).toHaveBeenCalled()
+	})
 })
--- test/Candle.test.ts
+++ test/Candle.test.ts
@@ -1,46 +1,47 @@
-import Candle from "../src/Candle"
-import moment from "moment"
+import Candle from '../src/Candle'
+import moment from 'moment'
+import { describe, expect, test } from '@jest/globals'
 
 describe('Candle', () =>
 {
-    test('should create a new Candle', () =>
-    {
-        const date: moment.Moment = moment()
-        const open: number = 15.18
-        const high: number = 15.27
-        const low: number = 15.17
-        const close: number = 15.25
-        const volume: number = 1689
+	test('should create a new Candle', () =>
+	{
+		const date: moment.Moment = moment()
+		const open = 15.18
+		const high = 15.27
+		const low = 15.17
+		const close = 15.25
+		const volume = 1689
 
-        const candle = new Candle(date, open, high, low, close, volume)
-        expect(candle).toBeInstanceOf(Candle)
-        expect(candle.date).toBe(date)
-        expect(candle.open).toBe(open)
-        expect(candle.high).toBe(high)
-        expect(candle.low).toBe(low)
-        expect(candle.close).toBe(close)
-        expect(candle.volume).toBe(volume)
-    })
+		const candle = new Candle(date, open, high, low, close, volume)
+		expect(candle).toBeInstanceOf(Candle)
+		expect(candle.date).toBe(date)
+		expect(candle.open).toBe(open)
+		expect(candle.high).toBe(high)
+		expect(candle.low).toBe(low)
+		expect(candle.close).toBe(close)
+		expect(candle.volume).toBe(volume)
+	})
 })
 
 describe('Candle.fromArr', () =>
 {
-    test('should create a new Candle from an array of numbers', () =>
-    {
-        const date: moment.Moment = moment()
-        const open: number = 15.18
-        const high: number = 15.27
-        const low: number = 15.17
-        const close: number = 15.25
-        const volume: number = 1689
+	test('should create a new Candle from an array of numbers', () =>
+	{
+		const date: moment.Moment = moment()
+		const open = 15.18
+		const high = 15.27
+		const low = 15.17
+		const close = 15.25
+		const volume = 1689
 
-        const candle = Candle.fromArr([date.valueOf(), open, high, low, close, volume])
-        expect(candle).toBeInstanceOf(Candle)
-        expect(candle.date.valueOf()).toBe(date.valueOf())
-        expect(candle.open).toBe(open)
-        expect(candle.high).toBe(high)
-        expect(candle.low).toBe(low)
-        expect(candle.close).toBe(close)
-        expect(candle.volume).toBe(volume)
-    })
+		const candle = Candle.fromArr([ date.valueOf(), open, high, low, close, volume ])
+		expect(candle).toBeInstanceOf(Candle)
+		expect(candle.date.valueOf()).toBe(date.valueOf())
+		expect(candle.open).toBe(open)
+		expect(candle.high).toBe(high)
+		expect(candle.low).toBe(low)
+		expect(candle.close).toBe(close)
+		expect(candle.volume).toBe(volume)
+	})
 })
