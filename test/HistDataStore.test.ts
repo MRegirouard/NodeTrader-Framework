65081b8f258dc777906ec91550692439e603e5a5
Ethan Girouard <mr.egirouard@gmail.com>
2023-03-27 23:59:41 -0400

    Lint HIstDataStore tests

--- test/HistDataStore.test.ts
+++ test/HistDataStore.test.ts
@@ -1,31 +1,36 @@
 
-import moment from "moment"
-import Candle from "../src/Candle"
-import CandleInterval, { IntervalUnit } from "../src/CandleInterval"
-import Candleset from "../src/Candleset"
-import HistDataStore from "../src/HistDataStore"
+import moment from 'moment'
+import Candle from '../src/Candle'
+import CandleInterval, { IntervalUnit } from '../src/CandleInterval'
+import Candleset from '../src/Candleset'
+import HistDataStore from '../src/HistDataStore'
 
 class TestHistDataStore extends HistDataStore
 {
 	public readonly tmpCandleStore: Candleset[] = []
 
-	public storeCandles(candles: Candleset): Promise<void> {
+	public storeCandles(candles: Candleset): Promise<void>
+	{
 		this.tmpCandleStore.push(candles)
 		return Promise.resolve()
 	}
 }
 
-describe("HistDataStore", () => {
-  test("should be of type HistDataStore", () => {
-	const source = new TestHistDataStore()
-	expect(source).toBeInstanceOf(HistDataStore)
-  })
+describe('HistDataStore', () =>
+{
+	test('should be of type HistDataStore', () =>
+	{
+		const source = new TestHistDataStore()
+		expect(source).toBeInstanceOf(HistDataStore)
+	})
 })
 
-describe("HistDataStore.storeCandles", () => {
-	test("should be able to store candles", async () => {
+describe('HistDataStore.storeCandles', () =>
+{
+	test('should be able to store candles', async() =>
+	{
 		const source = new TestHistDataStore()
-		const candleset = new Candleset(new CandleInterval(IntervalUnit.MINUTE, 1), { base: "BTC", trade: "USDT" })
+		const candleset = new Candleset(new CandleInterval(IntervalUnit.MINUTE, 1), { base: 'BTC', trade: 'USDT' })
 		candleset.addCandle(new Candle(moment(), 0, 0, 0, 0, 0))
 		candleset.addCandle(new Candle(moment(), 1, 1, 1, 1, 1))
 		await source.storeCandles(candleset)
@@ -36,17 +41,20 @@ describe("HistDataStore.storeCandles", () => {
 	})
 })
 
-describe("HistDataStore.storeCandleStream", () => {
-	test("should be able to store candles", async () => {
+describe('HistDataStore.storeCandleStream', () =>
+{
+	test('should be able to store candles', async() =>
+	{
 		const source = new TestHistDataStore()
-		const candleset = new Candleset(new CandleInterval(IntervalUnit.MINUTE, 1), { base: "BTC", trade: "USDT" })
-		
+		const candleset = new Candleset(new CandleInterval(IntervalUnit.MINUTE, 1), { base: 'BTC', trade: 'USDT' })
+
 		const stream = new ReadableStream<Candle>({
-			start(controller) {
+			start(controller): void
+			{
 				controller.enqueue(new Candle(moment(), 0, 0, 0, 0, 0))
 				controller.enqueue(new Candle(moment(), 1, 1, 1, 1, 1))
 				controller.close()
-			}
+			},
 		})
 
 		await source.storeCandleStream(stream, candleset)
