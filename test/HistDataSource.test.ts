0bfbcc6a83143dd5c8c425a28cd35ea3d76b29ed
Ethan Girouard <mr.egirouard@gmail.com>
2023-04-02 13:31:55 -0400

    Add tests for DataResponseType

--- test/HistDataSource.test.ts
+++ test/HistDataSource.test.ts
@@ -4,21 +4,41 @@ import Candleset from '../src/Candleset'
 import DataRequest from '../src/DataRequest'
 import Candle from '../src/Candle'
 import moment from 'moment'
+import DataResponseType from '../src/DataResponseType'
 
 class MyTestDataSource extends HistDataSource
 {
-	/* eslint class-methods-use-this: "off"*/
-	public streamRequest(req: DataRequest): ReadableStream<Candle>
+	// Flag for testing, wether to respond with a full or partial request
+	public doFullRequest = true
+
+	// Flag for testing, wether to respond with a response type or not
+	public sendResponseType = true
+
+	public streamRequest(req: DataRequest): ReadableStream<Candle | DataResponseType>
 	{
-		return new ReadableStream<Candle>({
+		const doFullRequest = this.doFullRequest
+		const sendResponseType = this.sendResponseType
+
+		return new ReadableStream<Candle | DataResponseType>({
 			start(controller): void
 			{
 				let remaining: number = req.count ?? 0
+
+				if (!doFullRequest)
+					remaining /= 2
 
 				const interval = setInterval(() =>
 				{
 					if (remaining <= 0)
 					{
+						if (sendResponseType)
+						{
+							if (doFullRequest)
+								controller.enqueue(DataResponseType.FullResponse)
+							else
+								controller.enqueue(DataResponseType.PartialResponse)
+						}
+
 						controller.close()
 						clearInterval(interval)
 					}
@@ -47,25 +67,73 @@ describe('HistDataSource.streamRequest', () =>
 	test('should be able to fullfill a streamRequest', async() =>
 	{
 		const source = new MyTestDataSource()
+		source.doFullRequest = true
+		source.sendResponseType = true
 		const candleset = new Candleset(new CandleInterval(IntervalUnit.MINUTE, 1), { base: 'BTC', trade: 'USD' })
 		const dataRequest = DataRequest.endCountRequest(candleset, 10, moment())
 		let count = 0
 		const reader = source.streamRequest(dataRequest).getReader()
 		await expect(reader.read().then(function processCandle({ done, value }): Promise<void> | void
 		{
-			expect((done || count < 10) && (!done || count === 10)).toBe(true)
-
 			if (done)
+			{
+				expect(count).toBe(10)
 				return
+			}
+			else if (value instanceof Candle)
+			{
+				expect(value).toBeDefined()
+				expect(count).toBeLessThan(10)
+				count++
+			}
+			else
+			{
+				expect(value).toBeDefined()
+				expect(count).toBe(10)
+				expect(value).toBe(DataResponseType.FullResponse)
+			}
 
-			expect(count).toBeLessThan(10)
-			expect(value).toBeDefined()
-			expect(value).toBeInstanceOf(Candle)
-			count++
 			return reader.read().then(processCandle)
 		})).resolves.toBeUndefined()
 
 		expect(count).toBe(10)
+	})
+
+	test('should be able to fullfill a streamRequest with a partial response', async() =>
+	{
+		const source = new MyTestDataSource()
+		source.doFullRequest = false
+		source.sendResponseType = true
+		const candleset = new Candleset(new CandleInterval(IntervalUnit.MINUTE, 1), { base: 'BTC', trade: 'USD' })
+		const dataRequest = DataRequest.endCountRequest(candleset, 10, moment())
+		let count = 0
+		const reader = source.streamRequest(dataRequest).getReader()
+		await expect(reader.read().then(function processCandle({ done, value }): Promise<void> | void
+		{
+			if (done)
+			{
+				expect(count).toBeLessThan(10)
+				expect(count).toBeGreaterThan(1)
+				return
+			}
+			else if (value instanceof Candle)
+			{
+				expect(value).toBeDefined()
+				expect(count).toBeLessThan(10)
+				count++
+			}
+			else
+			{
+				expect(value).toBeDefined()
+				expect(count).toBeLessThan(10)
+				expect(count).toBeGreaterThan(1)
+				expect(value).toBe(DataResponseType.PartialResponse)
+			}
+
+			return reader.read().then(processCandle)
+		})).resolves.toBeUndefined()
+
+		expect(count).toBeLessThan(10)
 	})
 })
 
@@ -74,10 +142,44 @@ describe('HistDataSource.fillRequest', () =>
 	test('should be able to fullfill a fillRequest', async() =>
 	{
 		const source = new MyTestDataSource()
+		source.doFullRequest = true
+		source.sendResponseType = true
 		const candleset = new Candleset(new CandleInterval(IntervalUnit.MINUTE, 1), { base: 'BTC', trade: 'USD' })
 		const dataRequest = DataRequest.endCountRequest(candleset, 10, moment())
 		const response = source.fillRequest(dataRequest)
-		await expect(response).resolves.toBeInstanceOf(Candleset)
-		await expect(response.then((set) => set.candles.length)).resolves.toBe(10)
+		await expect(response).resolves.toBeInstanceOf(Object)
+		await expect(response.then((set) => set[0])).resolves.toBeInstanceOf(Candleset)
+		await expect(response.then((set) => set[0].candles.length)).resolves.toBe(10)
+		await expect(response.then((set) => set[1])).resolves.toBe(DataResponseType.FullResponse)
+	})
+
+	test('should be able to fullfill a fillRequest with a partial response', async() =>
+	{
+		const source = new MyTestDataSource()
+		source.doFullRequest = false
+		source.sendResponseType = true
+		const candleset = new Candleset(new CandleInterval(IntervalUnit.MINUTE, 1), { base: 'BTC', trade: 'USD' })
+		const dataRequest = DataRequest.endCountRequest(candleset, 10, moment())
+		const response = source.fillRequest(dataRequest)
+		await expect(response).resolves.toBeInstanceOf(Object)
+		await expect(response.then((set) => set[0])).resolves.toBeInstanceOf(Candleset)
+		await expect(response.then((set) => set[0].candles.length)).resolves.toBeLessThan(10)
+		await expect(response.then((set) => set[0].candles.length)).resolves.toBeGreaterThan(1)
+		await expect(response.then((set) => set[1])).resolves.toBe(DataResponseType.PartialResponse)
+	})
+
+	test('should be able to fullfill a fillRequest when the stream does not send response type', async() =>
+	{
+		const source = new MyTestDataSource()
+		source.doFullRequest = false
+		source.sendResponseType = false
+		const candleset = new Candleset(new CandleInterval(IntervalUnit.MINUTE, 1), { base: 'BTC', trade: 'USD' })
+		const dataRequest = DataRequest.endCountRequest(candleset, 10, moment())
+		const response = source.fillRequest(dataRequest)
+		await expect(response).resolves.toBeInstanceOf(Object)
+		await expect(response.then((set) => set[0])).resolves.toBeInstanceOf(Candleset)
+		await expect(response.then((set) => set[0].candles.length)).resolves.toBeLessThan(10)
+		await expect(response.then((set) => set[0].candles.length)).resolves.toBeGreaterThan(1)
+		await expect(response.then((set) => set[1])).resolves.toBe(DataResponseType.FullResponse)
 	})
 })
