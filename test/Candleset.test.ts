d7ca387703453af59a9bd8cdef605e6f7760da4d
Ethan Girouard <mr.egirouard@gmail.com>
2023-08-30 11:38:53 -0400

    Modify/add tests for sorted addCandle

--- test/Candleset.test.ts
+++ test/Candleset.test.ts
@@ -4,6 +4,14 @@ import type TradingPair from '../src/TradingPair'
 import moment from 'moment'
 import Candle from '../src/Candle'
 
+function expectSorted(candles: readonly Candle[]): void
+{
+	for (let i = 1; i < candles.length; i++)
+	{
+		expect(candles[i].date.valueOf()).toBeGreaterThanOrEqual(candles[i - 1].date.valueOf())
+	}
+}
+
 describe('Candleset', () =>
 {
 	test('should be able to create a Candleset', () =>
@@ -64,6 +72,92 @@ describe('Candleset.addCandle', () =>
 		set.addCandle(candle3)
 
 		expect(set.candles).toHaveLength(3)
-		expect(set.candles).toEqual([ candle1, candle2, candle3 ])
+		expect(set.candles).toContain(candle1)
+		expect(set.candles).toContain(candle2)
+		expect(set.candles).toContain(candle3)
+	})
+	
+	test('should maintain the candles in sorted order when adding earlier candles', () =>
+	{
+		const interval = new CandleInterval(IntervalUnit.MINUTE, 1)
+		const pair: TradingPair = { base: 'USDT', trade: 'BTC' }
+		const set = new Candleset(interval, pair)
+
+		expect(set.candles).toEqual([])
+		expectSorted(set.candles)
+
+		const candle1 = new Candle(moment(), 22.45, 22.71, 22.41, 22.58, 148)
+		const candle2 = new Candle(candle1.date.clone().subtract(1, 'minute'), 22.58, 22.71, 22.41, 22.45, 148)
+		const candle3 = new Candle(candle2.date.clone().subtract(1, 'minute'), 22.41, 22.71, 22.45, 22.58, 148)
+
+		set.addCandle(candle1)
+		expectSorted(set.candles)
+		set.addCandle(candle2)
+		expectSorted(set.candles)
+		set.addCandle(candle3)
+		expectSorted(set.candles)
+	})
+
+	test('should maintain the candles in sorted order when adding later candles', () =>
+	{
+		const interval = new CandleInterval(IntervalUnit.MINUTE, 1)
+		const pair: TradingPair = { base: 'USDT', trade: 'BTC' }
+		const set = new Candleset(interval, pair)
+
+		expect(set.candles).toEqual([])
+		expectSorted(set.candles)
+
+		const candle1 = new Candle(moment(), 22.45, 22.71, 22.41, 22.58, 148)
+		const candle2 = new Candle(candle1.date.clone().add(1, 'minute'), 22.58, 22.71, 22.41, 22.45, 148)
+		const candle3 = new Candle(candle2.date.clone().add(1, 'minute'), 22.41, 22.71, 22.45, 22.58, 148)
+
+		set.addCandle(candle1)
+		expectSorted(set.candles)
+		set.addCandle(candle2)
+		expectSorted(set.candles)
+		set.addCandle(candle3)
+		expectSorted(set.candles)
+	})
+
+	test('should maintain the candles in sorted order when adding candles in the middle', () =>
+	{
+		const interval = new CandleInterval(IntervalUnit.MINUTE, 1)
+		const pair: TradingPair = { base: 'USDT', trade: 'BTC' }
+		const set = new Candleset(interval, pair)
+
+		expect(set.candles).toEqual([])
+		expectSorted(set.candles)
+
+		const firstCandle = new Candle(moment(), 22.45, 22.71, 22.41, 22.58, 148)
+		const lastCandle = new Candle(firstCandle.date.clone().add(10, 'minute'), 22.41, 22.71, 22.45, 22.58, 148)
+
+		set.addCandle(firstCandle)
+		expectSorted(set.candles)
+		set.addCandle(lastCandle)
+		expectSorted(set.candles)
+
+		for (let i = 1; i < 10; i++)
+		{
+			const candle = new Candle(firstCandle.date.clone().add(i, 'minute'), 22.58, 22.71, 22.41, 22.45, 148)
+			set.addCandle(candle)
+			expectSorted(set.candles)
+		}
+	})
+
+	test('should always maintain the candles in sorted order', () =>
+	{
+		const interval = new CandleInterval(IntervalUnit.MINUTE, 1)
+		const pair: TradingPair = { base: 'USDT', trade: 'BTC' }
+		const set = new Candleset(interval, pair)
+
+		expect(set.candles).toEqual([])
+		expectSorted(set.candles)
+
+		for (let i = 0; i < 100; i++)
+		{
+			const candle = new Candle(moment(), Math.random(), Math.random(), Math.random(), Math.random(), Math.random())
+			set.addCandle(candle)
+			expectSorted(set.candles)
+		}
 	})
 })
